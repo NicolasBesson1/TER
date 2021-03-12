9d9fff20bff02e3b84a2949840e2e318c1317666
NicolasBesson1 <58178659+NicolasBesson1@users.noreply.github.com>
2021-03-12 13:42:15 +0100

    Update AbstractSyntaxTree.py

--- AbstractSyntaxTree.py
+++ AbstractSyntaxTree.py
@@ -1,5 +1,6 @@
 from LexicalAnalysis import *
 from constants import *
+from z3 import *
 
 class AST():
 	def __init__(self, Val, Children):
@@ -13,8 +14,42 @@ class AST():
 		print(self.Val.val)
 		for c in self.Children:
 			c.DepthWalk()
-
-Example=AST(Lexema(EXQ,"exists"),[AST(Lexema(VAR,"x"),[]),AST(Lexema(VAR,"y"),[]), AST(Lexema(EQ,"="),[AST(Lexema(PLUS,"+"),[AST(Lexema(VAR,"x"),[]),AST(Lexema(VAR,"y"),[])]), AST(Lexema(VAR,"z"),[])]) ] )
+	def AddChild(self,Child):
+		self.Children.append(Child)
+	def ASTtoZ3(self):
+		if(self.Val.typ==EXQ):
+			l=len(self.Children)
+			A=self.Children[l-1].ASTtoZ3()
+			return Exists([Int(self.Children[i].Val.val) for i in range(l-2)],A)
+		if(self.Val.typ==NOT):
+			return Not(self.Children[0].ASTtoZ3())
+		if(self.Val.typ==PLUS):
+			return self.Children[0].ASTtoZ3() + self.Children[1].ASTtoZ3()
+		if(self.Val.typ==MINUS):
+			if(len(self.Children)==2):
+				return self.Children[0].ASTtoZ3() - self.Children[1].ASTtoZ3()
+			else:
+				return -self.Children[0].ASTtoZ3()
+		if(self.Val.typ==MULT):
+			return self.Children[0].ASTtoZ3() * self.Children[1].ASTtoZ3()
+		if(self.Val.typ==EQ):
+			return self.Children[0].ASTtoZ3() == self.Children[1].ASTtoZ3()
+		if(self.Val.typ==NE):
+			return self.Children[0].ASTtoZ3() != self.Children[1].ASTtoZ3()
+		if(self.Val.typ==LT):
+			return self.Children[0].ASTtoZ3() < self.Children[1].ASTtoZ3()
+		if(self.Val.typ==GT):
+			return self.Children[0].ASTtoZ3() > self.Children[1].ASTtoZ3()
+		if(self.Val.typ==LE):
+			return self.Children[0].ASTtoZ3() <= self.Children[1].ASTtoZ3()
+		if(self.Val.typ==GE):
+			return self.Children[0].ASTtoZ3() >= self.Children[1].ASTtoZ3()
+		if(self.Val.typ==AND):
+			return And(self.Children[0].ASTtoZ3(),self.Children[1].ASTtoZ3())
+		if(self.Val.typ==OR):
+			return Or(self.Children[0].ASTtoZ3(),self.Children[1].ASTtoZ3())
+		if(self.Val.typ==INT):
+			return int(self.Val.val)
+		if(self.Val.typ==VAR):
+			return Int(self.Val.val)
 		
-
-Example.DepthWalk()
